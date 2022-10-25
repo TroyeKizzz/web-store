f45f9c5c712bb87f1160cf4f1b7f95ce88c6e0d5
Sviatoslav Vasev <sviatoslav.vasev@tuni.fi>
2022-10-25 23:51:10 +0300

    Init project

---
+++ gameStore/src/main/java/com/example/gameStore/GameStoreIGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.gameStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStoreIGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameStoreIGameApplication.class, args);
+	}
+
+}
---
+++ gameStore/src/test/java/com/example/gameStore/GameStoreIGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gameStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStoreIGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
